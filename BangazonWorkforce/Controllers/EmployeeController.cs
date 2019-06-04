eab6edf5398d43576fb185075176a3980f32a79f
nikx1015 <nikx1015@gmail.com>
2019-06-04 10:50:18 -0400

    fixed error

--- BangazonWorkforce/Controllers/EmployeeController.cs
+++ BangazonWorkforce/Controllers/EmployeeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BangazonWorkforce.Models;
-using BangazonWorkforce.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
