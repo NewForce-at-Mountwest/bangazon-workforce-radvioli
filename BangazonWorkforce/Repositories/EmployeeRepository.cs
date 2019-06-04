c50a303332c58d1d0acfd3c8b2bb7b30d421e296
nikx1015 <nikx1015@gmail.com>
2019-06-04 14:12:38 -0400

    fixed error with links on nav

--- BangazonWorkforce/Repositories/EmployeeRepository.cs
+++ BangazonWorkforce/Repositories/EmployeeRepository.cs
@@ -80,9 +80,9 @@ namespace BangazonWorkforce.Repositories
                     cmd.CommandText = @"SELECT e.Id AS 'Employee Id', e.FirstName, e.LastName, e.IsSuperVisor, e.DepartmentId,
                         d.Id AS 'Department Id', d.Name AS 'Department', d.Budget ,c.Id AS 'Computer Id', tp.Name, tp.startDate, tp.endDate,
 						c.Make, c.Manufacturer, c.PurchaseDate, c.DecomissionDate, tp.Id AS 'Training Id'
-                        FROM Employee e LEFT JOIN Department d ON e.DepartmentId = d.Id
-						JOIN ComputerEmployee ce ON e.Id = ce.EmployeeId
-                        LEFT JOIN Computer c ON ce.ComputerId=c.Id LEFT JOIN EmployeeTraining et ON e.Id = et.EmployeeId LEFT JOIN TrainingProgram tp ON et.TrainingProgramId = tp.Id WHERE e.Id = @id ";
+                        FROM Employee e JOIN Department d ON e.DepartmentId = d.Id
+						LEFT JOIN ComputerEmployee ce ON e.Id = ce.EmployeeId
+                        LEFT JOIN Computer c ON ce.ComputerId=c.Id JOIN EmployeeTraining et ON e.Id = et.EmployeeId LEFT JOIN TrainingProgram tp ON et.TrainingProgramId = tp.Id WHERE e.Id = @id ";
                     cmd.Parameters.Add(new SqlParameter("@id", id));
                     SqlDataReader reader = cmd.ExecuteReader();
 
@@ -116,8 +116,6 @@ namespace BangazonWorkforce.Repositories
                         {
                             id = reader.GetInt32(reader.GetOrdinal("Training Id")),
                             name = reader.GetString(reader.GetOrdinal("Name")),
-                            //startDate = reader.GetDateTime(reader.GetOrdinal("Start Date")),
-                            //endDate = reader.GetDateTime(reader.GetOrdinal("End Date"))
                         };
 
                         employee.TrainingPrograms.Add(trainingProgram);
