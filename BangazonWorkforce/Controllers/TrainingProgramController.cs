f849fac20635c21d766232e28248aff73fff1c56
MattRowee <29.matt@gmail.com>
2019-06-06 11:58:27 -0400

    creating framework for Training Program component

---
+++ BangazonWorkforce/Controllers/TrainingProgramController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWorkforce.Models;
+using BangazonWorkforce.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonWorkforce.Controllers
+{
+    public class TrainingProgramController : Controller
+    {
+        public TrainingProgramController(IConfiguration config)
+        {
+            TrainingProgramRepository.SetConfig(config);
+        }
+        // GET: TrainingProgram
+        public ActionResult Index()
+        {
+            List<TrainingProgram> tpsReport = TrainingProgramRepository.GetTrainingPrograms();
+            return View();
+        }
+
+        // GET: TrainingProgram/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TrainingProgram/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TrainingProgram/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainingProgram/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainingProgram/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainingProgram/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainingProgram/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
