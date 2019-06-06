167e2534e6178ef6dc540b870c3cac4550ad593f
Dale <dalesauljr@gmail.com>
2019-06-06 13:40:40 -0400

    working GET all for training programs, created controller, view and repository for Training Program

--- BangazonWorkforce/Controllers/TrainingProgramController.cs
+++ BangazonWorkforce/Controllers/TrainingProgramController.cs
@@ -20,7 +20,7 @@ namespace BangazonWorkforce.Controllers
         public ActionResult Index()
         {
             List<TrainingProgram> tpsReport = TrainingProgramRepository.GetTrainingPrograms();
-            return View();
+            return View(tpsReport);
         }
 
         // GET: TrainingProgram/Details/5
